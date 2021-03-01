261e9cfe4feb1cd14db56fcb644b8f57e12d44cb
anil parmar <anilparmar76983@gmail.com>
2021-03-01 22:47:02 +0530

    bill ui done

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-'ok'
+<html>
+    <head>
+        <link href="media/bootstrap.min.css" rel="stylesheet">
+        <script src="media/jquery-1.11.1.min.js"></script>
+        <script src="media/bootstrap.min.js"></script>
+        <title>Choose Option</title>
+        
+    </head>
+    <body>
+        <a style="text-decoration:none" href="/debit_serch"><button type="button" class="btn btn-danger btn-lg btn-block">Debit</button></a>
+        <a style="text-decoration:none" href="/Credit_serch"><button type="button" class="btn  btn-lg btn-block">Credit</button></a>
+        <a style="text-decoration:none" href="/View_record"><button type="button" class="btn btn-success btn-lg btn-block">View Record By Time</button></a>
+    </body>
+</html>
